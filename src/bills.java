bf0832f955a9a06faa6e74797124c9a08aa7a1b8
Javi <olayon.javier@gmail.com>
2019-02-26 18:26:58 -0500

    declare private variables, create prompt

--- src/bills.java
+++ src/bills.java
@@ -1,10 +1,30 @@
 
+import javax.swing.JOptionPane;
+
+
 public class bills {
-	public static double hundreds(double change){
-		double output = 0;
-		while(change - 100 > 0){
-			output = change - 100;
+	private final double QUARTER = 0.25, DIME = 0.1, NICKEL = 0.05, PENNY = 0.01, THRESHOLD = 0.01;
+	
+	
+	public static void main(String[] args1){
+		double amt = Double.parseDouble(JOptionPane.showInputDialog("Please enter the amount you would like in change:"));
+		
+		makeChange(amt);
+	}
+	
+	public static void makeChange(double amount){
+		double trackAmount = 0.00;
+		while(!compareTo(amount, trackAmount)){
+			
 		}
-		return output;
+	}
+	
+	public boolean compareTo(double a, double b){
+		if(Math.abs(a - b) < THRESHOLD){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 }
